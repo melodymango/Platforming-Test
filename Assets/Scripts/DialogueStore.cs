dfd5073f88ecb309d2f31102f5d8c711fce5c207
melodymango <melodc2@uci.edu>
2017-11-30 16:35:39 -0800

    Swapped sprites, no more baby fat chocobo
    - Player now has Ana's sprite
    - Cornelia is added to teh game

---
+++ Assets/Scripts/DialogueStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueStore : MonoBehaviour
+{
+
+    public SortedDictionary<int, string> Lobby1; //Ana and Cornelia's coversation when Ana first arrives to the lobby
+
+    // Use this for initialization
+    void Start()
+    {
+        Lobby1.Add(1, "Kw...kwibbit?");
+    }
+
+}
